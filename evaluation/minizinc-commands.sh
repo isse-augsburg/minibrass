147cd7fa3a6baff553ec39731b10939d41a62939
Alexander Schiendorfer <schiendorfer@isse.de>
2016-01-19 14:41:31 +0100

    Some more commands

--- evaluation/minizinc-commands.sh
+++ evaluation/minizinc-commands.sh
@@ -14,4 +14,14 @@ minizinc -G or-tools -f fz-ort soft-queens-weighted.mzn
 
 # JaCoP
 mzn2fzn -G jacop soft-queens-weighted.mzn 
-java -cp $JAC_CP org.jacop.fz.Fz2jacop soft-queens-weighted.fzn
+fzn-jacop soft-queens-weighted.fzn
+
+# Now for minisearch 
+
+minisearch --solver fzn-gecode soft-queens.mzn soft-queens.dzn
+# not working due to set_in_reif ... missing in OR-tools
+minisearch --solver fzn-ort soft-queens.mzn soft-queens.dzn 
+# this one shows OR tools working with MiniSearch
+minisearch --solver fzn-ort golomb_mybab.mzn
+# it works in JaCoP as well
+minisearch --solver fzn-jacop soft-queens.mzn soft-queens.dzn
