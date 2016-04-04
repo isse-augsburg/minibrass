3e1716f8a6ece6f6e7b5911714ee45b563e3e0ce
Alexander Schiendorfer <alexander.schiendorfer@informatik.uni-augsburg.de>
2016-04-04 19:09:24 +0200

    Refactored to allow native BaB for OR-Tools as well

--- evaluation/Constraint-Relationships-Evaluation/src/de/isse/conf/Solver.java
+++ evaluation/Constraint-Relationships-Evaluation/src/de/isse/conf/Solver.java
@@ -2,7 +2,7 @@ package de.isse.conf;
 
 public enum Solver {
 	
-	GECODE("gecode","fzn-gecode"), JACOP("jacop","/home/alexander/Documents/Programs/minizinc/MiniZincIDE-2.0.13-bundle-linux-x86_64/fzn-jacop"), TOULBAR2, OR_TOOLS, CHOCO;
+	GECODE("gecode","fzn-gecode"), JACOP("jacop","fzn-jacop"), TOULBAR2, OR_TOOLS("or-tools","fzn-ort"), CHOCO, G12("g12_fd", "flatzinc");
 
 	private String mznGlobal;
 	private String fznExec;
