04354cdee7d850c3e80dc46ff70e1762b64567d1
Alexander Schiendorfer <alexander.schiendorfer@informatik.uni-augsburg.de>
2016-04-07 18:23:43 +0200

    Evaluation scripts

---
+++ evaluation/Constraint-Relationships-Evaluation/src/de/isse/eval/EvalConstants.java
@@ -0,0 +1,8 @@
+package de.isse.eval;
+
+import java.io.File;
+
+public class EvalConstants {
+	public static File RESULTS_DIR = new File("./results");
+	public static String CONNECTION_STRING = "jdbc:sqlite:../db/results.db";
+}
