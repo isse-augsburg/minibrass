9c8d482da030bba62c9de1df326ac2bc7a54932a
Alexander Schiendorfer <alexander.schiendorfer@informatik.uni-augsburg.de>
2016-07-15 14:40:31 +0200

    Direct products test and implementation

--- source-code/java/src/isse/mbr/model/parsetree/AbstractPVSInstance.java
+++ source-code/java/src/isse/mbr/model/parsetree/AbstractPVSInstance.java
@@ -2,12 +2,21 @@ package isse.mbr.model.parsetree;
 
 public abstract class AbstractPVSInstance {
 	protected String name;
-
+	protected String generatedBetterPredicate;
+	
 	public String getName() {
 		return name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getGeneratedBetterPredicate() {
+		return generatedBetterPredicate;
+	}
+
+	public void setGeneratedBetterPredicate(String generatedBetterPredicate) {
+		this.generatedBetterPredicate = generatedBetterPredicate;
 	} 
 }
