8f1cfd1adf5c92c14636c77c9db6d773eb789f58
Alexander Schiendorfer <alexander.schiendorfer@informatik.uni-augsburg.de>
2016-12-19 14:46:07 +0100

    Probabilistic cost function network type

---
+++ source-code/java/test/isse/mbr/integration/GlobalProbabilisticTest.java
@@ -0,0 +1,103 @@
+package isse.mbr.integration;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import isse.mbr.parsing.MiniBrassCompiler;
+import isse.mbr.parsing.MiniBrassParseException;
+
+/**
+ * This tests invokes MiniZinc for a probabilistic CSP
+ * It uses the global type definition in "def.mbr"
+ * @author Alexander Schiendorfer
+ *
+ */
+public class GlobalProbabilisticTest {
+
+	String minibrassModel = "test-models/testProb.mbr";
+	String minibrassMorphedModel = "test-models/testProbMorphed.mbr";
+	String minibrassCompiled = "test-models/testProb_o.mzn";
+	String minizincModel = "test-models/testProb.mzn";
+	String minizincMorphedModel = "test-models/testProbMorphed.mzn";
+	String minizincMorphedSearchModel = "test-models/testProbSearched.mzn";
+	private MiniBrassCompiler compiler;
+	private MiniZincLauncher launcher;
+	
+	@Before
+	public void setUp() throws Exception {
+		compiler = new MiniBrassCompiler();
+		compiler.setMinizincOnly(true); // due to other complications with floats using minisearch
+		launcher = new MiniZincLauncher();
+	}
+
+	@Test
+	public void testProbabilistic() throws IOException, MiniBrassParseException {
+		// 1. compile minibrass file
+		File output = new File(minibrassCompiled);
+		compiler.compile(new File(minibrassModel), output);
+		Assert.assertTrue(output.exists());
+		
+		// 2. execute MiniZinc
+		
+		BasicTestListener listener = new BasicTestListener();
+		launcher.addMiniZincResultListener(listener);
+		launcher.runMiniZincModel(new File(minizincModel), null, 60);
+		
+		// 3. check solution
+		Assert.assertTrue(listener.isSolved());
+		Assert.assertTrue(listener.isOptimal());
+		
+	
+		// for the objective, we need to find out the variable name 
+		Assert.assertEquals("1.0", listener.getObjectives().get("topLevelObjective"));
+	}
+	
+	@Test
+	public void testProbabilisticMorphed() throws IOException, MiniBrassParseException {
+		// 1. compile minibrass file
+		File output = new File(minibrassCompiled);
+		compiler.compile(new File(minibrassMorphedModel), output);
+		Assert.assertTrue(output.exists());
+		
+		// 2. execute MiniZinc
+		
+		BasicTestListener listener = new BasicTestListener();
+		launcher.addMiniZincResultListener(listener);
+		launcher.runMiniZincModel(new File(minizincMorphedModel), null, 60);
+		
+		// 3. check solution
+		Assert.assertTrue(listener.isSolved());
+		Assert.assertTrue(listener.isOptimal());
+		
+	
+		// for the objective, we need to find out the variable name 
+		Assert.assertEquals("0", listener.getObjectives().get("topLevelObjective"));
+	}
+	
+	@Test
+	public void testProbabilisticMorphedMinisearch() throws IOException, MiniBrassParseException {
+		// 1. compile minibrass file
+		File output = new File(minibrassCompiled);
+		compiler.setMinizincOnly(false);
+		compiler.compile(new File(minibrassMorphedModel), output);
+		Assert.assertTrue(output.exists());
+		
+		// 2. execute MiniZinc
+		
+		BasicTestListener listener = new BasicTestListener();
+		launcher.addMiniZincResultListener(listener);
+		launcher.runMiniSearchModel(new File(minizincMorphedSearchModel), null, 60);
+		
+		// 3. check solution
+		Assert.assertTrue(listener.isSolved());
+		Assert.assertTrue(listener.isOptimal());
+		
+	
+		// for the objective, we need to find out the variable name 
+		Assert.assertEquals("0", listener.getObjectives().get("topLevelObjective"));
+	}
+}
---
+++ source-code/java/test/isse/mbr/integration/ProbabilisticCostFunctionNetwork.java
@@ -0,0 +1,64 @@
+package isse.mbr.integration;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import isse.mbr.parsing.MiniBrassCompiler;
+import isse.mbr.parsing.MiniBrassParseException;
+
+/**
+ * This test case uses probabilistic cost function networks 
+ * and pure MiniZinc optimization
+ * @author Alexander Schiendorfer
+ *
+ */
+public class ProbabilisticCostFunctionNetwork {
+
+	String minibrassModel = "test-models/testProbCfn.mbr";
+	String minibrassCompiled = "test-models/testProbCfn_o.mzn";
+	String minizincModel = "test-models/testProbCfn.mzn";
+	private MiniBrassCompiler compiler;
+	private MiniZincLauncher launcher;
+	
+	@Before
+	public void setUp() throws Exception {
+		compiler = new MiniBrassCompiler();
+		compiler.setMinizincOnly(true);
+		launcher = new MiniZincLauncher();
+	}
+
+	@Test
+	public void test() throws IOException, MiniBrassParseException {
+		// 1. compile minibrass file
+		File output = new File(minibrassCompiled);
+
+		compiler.compile(new File(minibrassModel), output);
+		Assert.assertTrue(output.exists());
+		
+		// 2. execute minisearch
+		// Does not work due to float stuff
+//		
+//		BasicTestListener listener = new BasicTestListener();
+//		launcher.addMiniZincResultListener(listener);
+//		//launcher.runMiniSearchModel(new File(minizincModel), null, 60);
+//		launcher.runMiniZincModel(new File(minizincModel), null, 60);
+//		
+//		// 3. check solution
+//		Assert.assertTrue(listener.isSolved());
+//		Assert.assertTrue(listener.isOptimal());
+//		
+//		Assert.assertEquals("1", listener.getLastSolution().get("x"));
+//		Assert.assertEquals("2", listener.getLastSolution().get("y"));
+//		Assert.assertEquals("1", listener.getLastSolution().get("z"));
+//		
+//		// for the objective, we need to find out the variable name 
+//		// instance was "cr1"
+//		String obj = "topLevelObjective";
+//		Assert.assertEquals("1", listener.getObjectives().get(obj));
+	}
+
+}
