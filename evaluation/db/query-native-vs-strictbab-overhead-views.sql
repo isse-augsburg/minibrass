0b2c4c70b75711c1ed5a7e42cdaea819dea4766a
Alexander Schiendorfer <alexander.schiendorfer@informatik.uni-augsburg.de>
2017-07-14 16:40:17 +0200

    Evaluation minor changes

--- evaluation/db/query-native-vs-strictbab-overhead-views.sql
+++ evaluation/db/query-native-vs-strictbab-overhead-views.sql
@@ -1,36 +1,28 @@
-DROP View IF exists PvsData; Drop View IF exists NativeData; Drop View IF exists PvsNativeSummary; Drop View IF exists ToulbarData;
-
-
-Create TEMPORARY View IF NOT EXISTS ToulbarData as 
-SELECT problem, instance, elapsedSecs, Objective, Optimally, cf.ID as ConfigId FROM 
-JobResult jr INNER JOIN Config cf ON jr.ConfigId = cf.ID 
-INNER JOIN Solver sv ON jr.SolverId = sv.ID 
-where SPD = 1 and MIF = 0 and SearchType = 3 and sv.ID = 2
-order by problem, instance, elapsedSecs, solverId ;
+DROP View IF exists PvsData; Drop View IF exists NativeData; Drop View IF exists PvsNativeSummary;
 
 Create TEMPORARY View IF NOT EXISTS PvsData as 
-SELECT problem, instance, SolverName, SolverId,elapsedSecs, Objective, Optimally, cf.ID as ConfigId FROM 
+SELECT problem, instance, SolverName, SolverId, round(elapsedSecs,2) as elapsedSecs, Objective, Optimally, cf.ID as ConfigId FROM 
 JobResult jr INNER JOIN Config cf ON jr.ConfigId = cf.ID 
 INNER JOIN Solver sv ON jr.SolverId = sv.ID 
-where SPD = 1 and MIF = 0 and SearchType = 1 and Solved = 1
+where SPD = 1 and MIF = 1 and SearchType = 1 and Solved = 1
 order by problem, instance, elapsedSecs, solverId ;
 
 Create TEMPORARY View  IF NOT EXISTS NativeData as 
-SELECT problem, instance, SolverName, SolverId, elapsedSecs, Objective, Optimally, cf.ID as ConfigId FROM 
+SELECT problem, instance, SolverName, SolverId, round(elapsedSecs,2) as elapsedSecs, Objective, Optimally, cf.ID as ConfigId FROM 
 JobResult jr INNER JOIN Config cf ON jr.ConfigId = cf.ID 
 INNER JOIN Solver sv ON jr.SolverId = sv.ID 
 where SPD = 1 and MIF = 0 and SearchType = 3 
 order by problem, instance, elapsedSecs, solverId ;
 
 Create TEMPORARY View IF NOT EXISTS PvsNativeSummary as 
-Select pd.Problem, pd.Instance, pd.SolverName, pd.elapsedSecs as "SmythElapsed", pd.Objective as "SmythObj", nd.elapsedSecs as "WeightsElapsed", td.elapsedSecs as "ToulbarElapsed", nd.Objective as "WeightsObjective" ,
-       pd.elapsedSecs - nd.elapsedSecs as "AbsOverhead", pd.elapsedSecs / nd.elapsedSecs as "RelOverhead"
+Select pd.Problem, pd.Instance, pd.SolverName, pd.elapsedSecs as "SmythElapsed", pd.Objective as "SmythObj", nd.elapsedSecs as "WeightsElapsed", nd.Objective as "WeightsObjective" ,
+       pd.elapsedSecs - nd.elapsedSecs as "AbsOverhead", 
+	   pd.elapsedSecs / nd.elapsedSecs as "RelOverhead",
+	   pd.elapsedSecs < nd.elapsedSecs as "SmythWins" 
 
-from PvsData pd, NativeData nd, ToulbarData td
+from PvsData pd, NativeData nd
 where pd.Problem = nd.Problem and 
       pd.Instance = nd.Instance and 
-	  pd.SolverId = nd.SolverId and
-	  pd.Problem = td.Problem and pd.Instance = td.Instance
+	  pd.SolverId = nd.SolverId
 order by pd.problem, pd.instance, pd.solverName;
-
 
