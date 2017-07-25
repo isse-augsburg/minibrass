b141cee03efffa065e20795d8e4a2f221a830778
Alexander Schiendorfer <alexander.schiendorfer@informatik.uni-augsburg.de>
2017-07-26 00:20:47 +0200

    Encoding of multisets

--- evaluation/db/query-native-vs-strictbab-overhead-views.sql
+++ evaluation/db/query-native-vs-strictbab-overhead-views.sql
@@ -4,7 +4,7 @@ Create TEMPORARY View IF NOT EXISTS PvsData as
 SELECT problem, instance, SolverName, SolverId, round(elapsedSecs,2) as elapsedSecs, Objective, Optimally, cf.ID as ConfigId FROM 
 JobResult jr INNER JOIN Config cf ON jr.ConfigId = cf.ID 
 INNER JOIN Solver sv ON jr.SolverId = sv.ID 
-where SPD = 1 and MIF = 1 and SearchType = 1 and Solved = 1
+where SPD = 1 and MIF = 0 and SearchType = 1 and Solved = 1
 order by problem, instance, elapsedSecs, solverId ;
 
 Create TEMPORARY View  IF NOT EXISTS NativeData as 
@@ -17,8 +17,7 @@ order by problem, instance, elapsedSecs, solverId ;
 Create TEMPORARY View IF NOT EXISTS PvsNativeSummary as 
 Select pd.Problem, pd.Instance, pd.SolverName, pd.elapsedSecs as "SmythElapsed", pd.Objective as "SmythObj", nd.elapsedSecs as "WeightsElapsed", nd.Objective as "WeightsObjective" ,
        pd.elapsedSecs - nd.elapsedSecs as "AbsOverhead", 
-	   pd.elapsedSecs / nd.elapsedSecs as "RelOverhead",
-	   pd.elapsedSecs < nd.elapsedSecs as "SmythWins" 
+	   pd.elapsedSecs / nd.elapsedSecs as "RelOverhead"
 
 from PvsData pd, NativeData nd
 where pd.Problem = nd.Problem and 
