3c9c65c4c6b8b4bce2a1c01e96153cf813658fb3
ChristopherS <stifter.christopher@gmail.com>
2017-10-10 15:38:57 +0200

    add examples

--- source-code/minibrassgui/src/main/scala/HelloStageDemo.scala
+++ source-code/minibrassgui/src/main/scala/HelloStageDemo.scala
@@ -1,7 +1,10 @@
 import scalafx.application
 import scalafx.Includes._
 import scalafx.application.JFXApp
+import scalafx.beans.property.DoubleProperty
 import scalafx.scene.Scene
+import scalafx.scene.control.Alert
+import scalafx.scene.control.Alert.AlertType
 import scalafx.scene.paint.Color._
 import scalafx.scene.shape.Rectangle
 
@@ -18,7 +21,51 @@ object HelloStageDemo extends JFXApp {
         width = 100
         height = 100
         fill <== when (hover) choose Green otherwise Red
+        onMouseEntered = { anything => println("ok seems to work")}
       }
     }
   }
+  val speed = new DoubleProperty(this, "speed", 55)
+  val subscription = speed.onChange { (_, oldValue, newValue) =>
+    println(s"Value of property 'speed' is changing from $oldValue to $newValue\n")
+  }
+
+  speed() = 60
+  subscription.cancel()
+  speed.value = 25
+
+  val base = DoubleProperty(15)
+  val height = DoubleProperty(10)
+  val area = DoubleProperty(0)
+
+  area <== base * height / 2
+  printValues()
+
+  println("base to 20")
+  base() = 20
+  printValues()
+
+  println("height to 5")
+  height() = 5
+  printValues()
+
+
+  new Alert(AlertType.Information, "Hello Test").showAndWait()
+
+
+  def printValues(): Unit = {
+    println(f"base = ${base()}%4.1f, height = ${height()}%4.1f, area = ${area()}%5.1f\n")
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
