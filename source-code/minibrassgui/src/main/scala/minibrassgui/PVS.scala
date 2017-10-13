638b960b187a843339c42ec618cd797bb153d615
ChristopherS <stifter.christopher@gmail.com>
2017-10-13 11:23:42 +0200

    extend PVS definitions

--- source-code/minibrassgui/src/main/scala/minibrassgui/PVS.scala
+++ source-code/minibrassgui/src/main/scala/minibrassgui/PVS.scala
@@ -1,25 +1,63 @@
 package minibrassgui
 
+import minibrassgui.PVSComp.CompParts
 
-class CostFunctionNetwork(name: String) extends PVSLeaf(name)
-class MaxCSP(name: String) extends PVSLeaf(name)
-class ConstraintPreferences(name: String, useSPD: Boolean) extends PVSLeaf(name)
-class WeightedCSP(name: String) extends PVSLeaf(name)
-class PropabilisticCSP(name: String) extends PVSLeaf(name)
-class FuzzyCSP(name: String) extends PVSLeaf(name)
+/**
+  * The following case classes are all available PVS types.
+ *
+  * @param name Name of the PVS
+  */
+case class CostFunctionNetwork(name: String) extends PVSLeaf(name) {
+  override def visualiseEntity(): Any = ???
+}
+case class MaxCSP(name: String) extends PVSLeaf(name) {
+  override def visualiseEntity(): Any = ???
+}
+case class ConstraintPreferences(name: String, useSPD: Boolean) extends PVSLeaf(name){
+  override def visualiseEntity(): Any = ???
+}
+case class WeightedCSP(name: String) extends PVSLeaf(name){
+  override def visualiseEntity(): Any = ???
+}
+case class ProbabilisticCSP(name: String) extends PVSLeaf(name){
+  override def visualiseEntity(): Any = ???
+}
+case class FuzzyCSP(name: String) extends PVSLeaf(name){
+  override def visualiseEntity(): Any = ???
+}
 
 
 /**
   * A PVS is either a Leaf...
   */
-abstract class PVSLeaf(name: String) extends PVS(name)
+abstract class PVSLeaf(name: String) extends PVS(name){
+  override def visualiseEntity(): Any = ???
+}
 
 /**
-  * ...or a Composition of PVS
+  * ...or a Composition of PVS's
+  * @param consistsOf A PVSComp consists of two PVS's. Right after creation of 'this', these do not yet exist.
   */
-abstract class PVSComp(name: String) extends PVS(name)
+case class PVSComp(name: String, consistsOf: Option[CompParts] = None) extends PVS(name){
+  override def visualiseEntity(): Any = ???
+
+  def getChildren(): Option[CompParts] = consistsOf
+
+  def setChildren(compParts: CompParts) = PVSComp(name, Some(compParts))
+}
+
+object PVSComp {
+  type CompParts = (PVS, PVS)
+}
 
 /**
   * Basic PVS class.
   */
-sealed abstract class PVS(name: String)
+sealed abstract class PVS(name: String) extends Visualise
+
+/**
+  * Every PVS must be visualised. The trait's function should return an appropriate graphical element.
+  */
+abstract trait Visualise {
+  def visualiseEntity() : Any
+}
