bf62b56c2b48e861330ac13d3082edd51248d380
Alexander Schiendorfer <alexander.schiendorfer@informatik.uni-augsburg.de>
2017-11-05 16:23:13 +0100

    Output of condorcet method, applied to lunch deselection

--- source-code/java/src/isse/mbr/tools/PairwiseComparator.java
+++ source-code/java/src/isse/mbr/tools/PairwiseComparator.java
@@ -3,6 +3,7 @@ package isse.mbr.tools;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -189,26 +190,41 @@ public class PairwiseComparator {
 	}
 	
 	public String getHtml() {
-		String rowColor = "MediumSeaGreen";
-		String colColor = "#ffb366";
-		String neutralColor = "#ECF0F1";
+		String rowStyle = "leftBetter"; // "MediumSeaGreen";
+		String colStyle = "rightBetter"; // "#ffb366";
+		String neutralStyle = "neutral"; // "#ECF0F1";
 		
-		StringBuilder sb = new StringBuilder("<table>\n");
+		// for now, manual shading
+		String rowColor = "rgba(81, 86, 83, %s)";
+		String colColor = "rgba(240, 127, 22, %s)";
+		
+		StringBuilder sb = new StringBuilder("<table class=\"condorcetTable\">\n");
 		sb.append("<tr><th></th>\n");
 		for(int i = 0; i < solutions.size(); ++i) {
-			sb.append(String.format("<th style=\"background-color:%s;\">%s</th>", colColor, solutions.get(i) ));
+			sb.append(String.format("<th><span class=\"%s\">%s</span></th>", colStyle, solutions.get(i) ));
 		}
 		sb.append("</tr>\n");
+		DecimalFormat df = new DecimalFormat("##.00");
 		for(int i = 0; i < solutions.size(); ++i) {
-			sb.append(String.format("<tr><td style=\"background-color:%s;\"><b>%s</b></td>\n", rowColor, solutions.get(i)));
+			sb.append(String.format("<tr><td><span class=\"%s solStyle\">%s</span></td>\n", rowStyle, solutions.get(i)));
 			for(int j = 0; j < solutions.size(); ++j) {
-				String color = neutralColor;
-				if(leftLikes[i][j] > rightLikes[i][j])
-					color = rowColor;
-				if(rightLikes[i][j] > leftLikes[i][j])
-					color = colColor;
+				String color = neutralStyle;
+				String styleShading = "";
 				
-				sb.append(String.format("<td style=\"background-color:%s;\">%d / %d / %d </td>", color, leftLikes[i][j], indifferents[i][j], rightLikes[i][j]));
+				int total = leftLikes[i][j] + rightLikes[i][j] + indifferents[i][j];
+				
+				if(leftLikes[i][j] > rightLikes[i][j]) {
+					color = rowStyle;
+					double ratio = (double) leftLikes[i][j]	/ total;
+					styleShading =String.format("style =\" background-color: "+rowColor +"\"", df.format(ratio));
+				}
+				if(rightLikes[i][j] > leftLikes[i][j]) {
+					color = colStyle;					
+					double ratio = (double) rightLikes[i][j]	/ total;
+					styleShading =String.format("style =\" background-color: "+colColor +"\"", df.format(ratio));
+				}
+				
+				sb.append(String.format("<td><span class=\"%s\" %s>%d / %d / %d</span></td>", color, styleShading, leftLikes[i][j], indifferents[i][j], rightLikes[i][j]));
 			}
 			sb.append("</tr>\n");
 		}		
