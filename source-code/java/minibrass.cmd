1ab011e399cef1dd3f5b9fa2d65a51d862f63f54
Alexander Schiendorfer <alexander.schiendorfer@informatik.uni-augsburg.de>
2019-01-23 16:20:29 +0100

    New command line interface

---
+++ source-code/java/minibrass.cmd
@@ -0,0 +1,1 @@
+java -jar %~dp0\mbr2mzn.jar %*
--- source-code/java/src/isse/mbr/tools/execution/MiniBrassRunner.java
+++ source-code/java/src/isse/mbr/tools/execution/MiniBrassRunner.java
@@ -1,14 +1,28 @@
 package isse.mbr.tools.execution;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 
 import isse.mbr.parsing.MiniBrassCompiler;
+import isse.mbr.parsing.MiniBrassCompiler.StdoutConsoleHandler;
 import isse.mbr.parsing.MiniBrassParseException;
 import isse.mbr.parsing.MiniBrassParser;
 
@@ -29,6 +43,9 @@ public class MiniBrassRunner {
 	private int modelIndex;
 	private File originalMiniZincFile;
 	private List<MiniZincSolution> allSolutions;
+	private HelpFormatter formatter;
+	private Options options;
+	private static Logger logger;
 	
 	public MiniBrassRunner() {
 		miniZincRunner = new MiniZincRunner();
@@ -87,6 +104,107 @@ public class MiniBrassRunner {
 		return lastSolution;
 	}
 
+	public static void main(String[] args) throws SecurityException, IOException {
+		logger = Logger.getGlobal();
+		logger.setLevel(Level.FINER);
+		System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tF %1$tT %4$s %2$s %5$s%6$s%n");
+
+		SimpleFormatter formatterTxt = new SimpleFormatter();
+		FileHandler logFile = new FileHandler("log2.txt");
+		logFile.setLevel(Level.FINER);
+
+		logFile.setFormatter(formatterTxt);
+		logger.addHandler(logFile);
+
+		// Create and set handler
+		Handler systemOut = new StdoutConsoleHandler();
+		systemOut.setLevel(Level.ALL);
+
+		logger.addHandler(systemOut);
+		logger.setLevel(Level.SEVERE);
+
+		// Prevent logs from processed by default Console handler.
+		logger.setUseParentHandlers(false); // Solution 1
+
+		new MiniBrassRunner().doMain(args);
+	}
+	
+	private void printUsage() {
+		formatter.printHelp("minibrass [<options>] <minibrass-model>.mbr <minizinc-model>.mzn <minizinc-data>.dzn\n\nOptions:\n", options);
+	}
+	
+	private void doMain(String[] args) {
+		// create the command line parser
+		CommandLineParser parser = new DefaultParser();
+
+		options = new Options();
+		options.addOption("h", "help", false, "print this message");
+		options.addOption("d", "debug", false, "write intermediate files");
+
+		formatter = new HelpFormatter();
+		String minibrassFile = null;
+		String minizincModelFile = null;
+		String minizincDataFile = null;
+		List<File> dataFiles = new LinkedList<>();
+		
+		try {
+			// parse the command line arguments
+			CommandLine line = parser.parse(options, args);
+
+			List<String> argList = line.getArgList();
+			
+			if (line.hasOption('h')) {
+				printUsage();
+				System.exit(0);
+			}
+
+			if (argList.size() < 2) {
+				System.out.println("minibrass expects one MiniBrass file, one MiniZinc file and optionally some data files as input.");
+				printUsage();
+				System.exit(1);
+			} else {
+				minibrassFile = argList.get(0);
+				if (!minibrassFile.endsWith("mbr")) {
+					System.out.println("Warning: MiniBrass file ending on .mbr expected!");
+				}
+				
+				minizincModelFile = argList.get(1);
+				if (!minizincModelFile.endsWith("mzn")) {
+					System.out.println("Warning: MiniZinc file ending on .mzn expected!");
+				}
+				
+				if(argList.size() > 2) {
+					minizincDataFile = argList.get(2);
+					if (!minizincDataFile.endsWith("dzn")) {
+						System.out.println("Warning: MiniZinc data file ending on .dzn expected!");
+					}
+					dataFiles.add(new File(minizincDataFile));
+				}
+			}
+
+			if (line.hasOption("debug")) {
+				debug = true;				
+			} 
+
+			logger.info("Processing " + minibrassFile + " | " + minizincModelFile+ " | " + minizincDataFile +" to file.");
+			executeBranchAndBound(new File(minizincModelFile), new File(minibrassFile), dataFiles);
+			
+		} catch (ParseException exp) {
+			logger.severe("Unexpected exception:" + exp.getMessage());
+			printUsage();
+		} catch (FileNotFoundException e) {
+			logger.severe("File was not found");
+			e.printStackTrace();
+		} catch (MiniBrassParseException e) {
+			logger.severe("Could not parse MiniBrass model:");
+			e.printStackTrace();
+		} catch (IOException e) {
+			logger.severe("IO error: ");
+			e.printStackTrace();
+		}
+		
+	}
+	
 	private File appendMiniZincCode(File miniZincFile, String updatedConstraint) throws IOException {
 		if (writeIntermediateFiles) {
 			String name = miniZincFile.getName();
