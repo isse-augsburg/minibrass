eabd3cf41c26e01e2a0900c28941f0d7b408cbec
Elias Keis <git-commits@elkei.de>
2021-05-12 14:52:31 +0200

    extract main method from MiniBrassRunner

--- source-code/java/src/isse/mbr/tools/execution/MiniBrassRunner.java
+++ source-code/java/src/isse/mbr/tools/execution/MiniBrassRunner.java
@@ -1,30 +1,17 @@
 package isse.mbr.tools.execution;
 
 import isse.mbr.parsing.MiniBrassCompiler;
-import isse.mbr.parsing.MiniBrassCompiler.StdoutConsoleHandler;
 import isse.mbr.parsing.MiniBrassParseException;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
-import java.util.logging.FileHandler;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -54,15 +41,13 @@ public class MiniBrassRunner {
 	private String lastSolvableMiniZincModel;
 	private String workingMiniZincModel;
 	private List<MiniZincSolution> allSolutions;
-	private static Logger logger;
 
 	public MiniBrassRunner() {
 		this(new MiniZincConfiguration());
 	}
 
 	public MiniBrassRunner(MiniZincConfiguration configuration) {
-		configuration.setUseAllSolutions(false);
-		miniZincRunner.setConfiguration(configuration);
+		setMiniZincConfiguration(configuration);
 	}
 
 	private void initializeBranchAndBound(File miniZincFile) throws IOException {
@@ -117,131 +102,6 @@ public class MiniBrassRunner {
 		}
 		cleanup(workingMiniZincFile);
 		return lastSolution;
-	}
-
-	public static void main(String[] args) throws SecurityException, IOException {
-		logger = Logger.getGlobal();
-		logger.setLevel(Level.FINER);
-		System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tF %1$tT %4$s %2$s %5$s%6$s%n");
-
-		SimpleFormatter formatterTxt = new SimpleFormatter();
-		FileHandler logFile = new FileHandler("log2.txt");
-		logFile.setLevel(Level.FINER);
-
-		logFile.setFormatter(formatterTxt);
-		logger.addHandler(logFile);
-
-		// Create and set handler
-		Handler systemOut = new StdoutConsoleHandler();
-		systemOut.setLevel(Level.ALL);
-
-		logger.addHandler(systemOut);
-		logger.setLevel(Level.SEVERE);
-
-		// Prevent logs from processed by default Console handler.
-		logger.setUseParentHandlers(false); // Solution 1
-
-		new MiniBrassRunner().doMain(args);
-	}
-
-	private void printUsage(Options options) {
-		new HelpFormatter().printHelp(
-				"minibrass [<options>] <minibrass-model>.mbr <minizinc-model>.mzn <minizinc-data>.dzn\n\nOptions:\n",
-				options);
-	}
-
-	private void doMain(String[] args) {
-		// create the command line parser
-		CommandLineParser parser = new DefaultParser();
-
-		Options options = new Options()
-				.addOption("h", "help", false, "print this message")
-				.addOption("d", "debug", false, "write intermediate files")
-				.addOption("w", "weak-opt", false, "only use non-domination search")
-				.addOption("r", "random-seed", true, "initial random seed for branch-and-bound")
-				.addOption("s", "solver", true, "solver to use for branch-and-bound")
-				.addOption("t", "timeout", true, "timeout in milliseconds");
-
-		String minibrassFile = null;
-		String minizincModelFile = null;
-		String minizincDataFile = null;
-		List<File> dataFiles = new LinkedList<>();
-
-		try {
-			// parse the command line arguments
-			CommandLine line = parser.parse(options, args);
-
-			List<String> argList = line.getArgList();
-
-			if (line.hasOption('h')) {
-				printUsage(options);
-				System.exit(0);
-			}
-
-			if (line.hasOption("solver")) {
-				miniZincRunner.getConfiguration().setSolverId(line.getOptionValue("solver"));
-			}
-
-			if (line.hasOption("timeout")) {
-				miniZincRunner.getConfiguration().setTimeout(Integer.parseInt(line.getOptionValue("timeout")));
-			}
-
-			if (line.hasOption("random-seed")) {
-				setInitialRandomSeed(Integer.parseInt(line.getOptionValue("random-seed")));
-			}
-
-			if (line.hasOption("weak-opt")) {
-				dominationSearch = false;
-			}
-
-			if (argList.size() < 2) {
-				System.out.println(
-						"minibrass expects one MiniBrass file, one MiniZinc file and optionally some data files as input.");
-				printUsage(options);
-				System.exit(1);
-			} else {
-				minibrassFile = argList.get(0);
-				if (!minibrassFile.endsWith("mbr")) {
-					System.out.println("Warning: MiniBrass file ending on .mbr expected!");
-				}
-
-				minizincModelFile = argList.get(1);
-				if (!minizincModelFile.endsWith("mzn")) {
-					System.out.println("Warning: MiniZinc file ending on .mzn expected!");
-				}
-
-				if (argList.size() > 2) {
-					minizincDataFile = argList.get(2);
-					if (!minizincDataFile.endsWith("dzn")) {
-						System.out.println("Warning: MiniZinc data file ending on .dzn expected!");
-					}
-					dataFiles.add(new File(minizincDataFile));
-				}
-			}
-
-			if (line.hasOption("debug")) {
-				debug = true;
-				miniZincRunner.setDebug(true);
-			}
-
-			logger.info(
-					"Processing " + minibrassFile + " | " + minizincModelFile + " | " + minizincDataFile + " to file.");
-			executeBranchAndBound(new File(minizincModelFile), new File(minibrassFile), dataFiles);
-
-		} catch (ParseException exp) {
-			logger.severe("Unexpected exception:" + exp.getMessage());
-			printUsage(options);
-		} catch (FileNotFoundException e) {
-			logger.severe("File was not found");
-			e.printStackTrace();
-		} catch (MiniBrassParseException e) {
-			logger.severe("Could not parse MiniBrass model:");
-			e.printStackTrace();
-		} catch (IOException e) {
-			logger.severe("IO error: ");
-			e.printStackTrace();
-		}
-
 	}
 
 	private void appendMiniZincCode(String additionalCode) throws IOException {
@@ -302,6 +162,7 @@ public class MiniBrassRunner {
 	}
 
 	public void setMiniZincConfiguration(MiniZincConfiguration configuration) {
+		configuration.setUseAllSolutions(false);
 		miniZincRunner.setConfiguration(configuration);
 	}
 
@@ -311,6 +172,7 @@ public class MiniBrassRunner {
 
 	public void setDebug(boolean debug) {
 		this.debug = debug;
+		this.miniZincRunner.setDebug(debug);
 	}
 
 	public List<MiniZincSolution> getAllSolutions() {
